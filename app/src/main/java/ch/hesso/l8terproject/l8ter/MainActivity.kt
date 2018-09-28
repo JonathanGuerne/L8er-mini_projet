24b3614b9d372bc846b57470268a4365bf016670
Jonathan Guerne <jonathan.guerne@gmail.com>
2018-09-28 09:34:15 +0200

    creating project

---
+++ app/src/main/java/ch/hesso/l8terproject/l8ter/MainActivity.kt
@@ -0,0 +1,12 @@
+package ch.hesso.l8terproject.l8ter
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
